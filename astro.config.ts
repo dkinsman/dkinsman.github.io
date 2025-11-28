8b57b7d015aaa3d9e5a39f2ebf8567db6301fe8d
Dawson Kinsman <60490532+dkinsman@users.noreply.github.com>
2025-11-27 19:11:17 -0500

    try to fix 404

--- astro.config.ts
+++ astro.config.ts
@@ -20,6 +20,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://dkinsman.github.io/',
+  base: '/', 
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
