79f0d721a08f51372a8cdee5708b6a7c759115fe
Dawson Kinsman <60490532+dkinsman@users.noreply.github.com>
2025-12-01 12:43:08 -0500

    test2

--- astro.config.ts
+++ astro.config.ts
@@ -19,7 +19,7 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://dkinsman.github.io/',
+  site: 'https://github.com/dkinsman/dkinsman.github.io',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
